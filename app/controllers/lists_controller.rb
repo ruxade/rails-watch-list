ffb1f33aa7994708028c23672c31d4af1ba66300
Adelina Gutu <w1705972@gmail.com>
2024-09-27 16:37:00 +0100

    ?

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,7 +15,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path, notice: 'List was successfully created.'
+      redirect_to lists_path, notice: 'List was  created.'
     else
       render :new
     end
